2576a8871104a083f2dd61b3a2c93382d2222ee2
Taylor King <dekkofilms@gmail.com>
2016-09-23 15:08:59 -0500

    added create for posts and comments with testing

--- server.js
+++ server.js
@@ -11,10 +11,43 @@ var knex = require('knex')(config);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 app.set('view engine', 'ejs');
+// app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('booyah');
+});
+
+app.get('/posts', function (req, res) {
+  knex.select().table('posts').then(function (data) {
+    res.render('posts/index', {posts: data});
+  })
+});
+
+app.post('/posts', function (req, res) {
+  var post = req.body;
+  knex('posts').insert(post).then(function () {
+      res.redirect('/posts');
+  });
+});
+
+app.get('/posts/:postID', function (req, res) {
+  var postID = req.params.postID;
+  var url = req.url
+  knex('posts').where({id: postID}).first().then(function (post) {
+    knex('comments').where({post_id: postID}).then(function (comment) {
+      res.render('posts/id', {post: post, url: url, comment: comment});
+    })
+  });
+});
+
+app.post('/posts/:postID', function (req, res) {
+  var postID = req.params.postID;
+  var comment = req.body;
+  knex('comments').insert({comment_content: comment.comment_content, post_id: postID}).then(function () {
+    res.redirect(req.url);
+  });
 });
 
 app.get('/users', function (req, res) {
--- test/mypage_test.js
+++ test/mypage_test.js
@@ -16,10 +16,68 @@ describe('find initial route', function() {
   });
 });
 
+describe('find new post page', function() {
+
+  it('should go to post page', function(done) {
+    request(app).get('/posts')
+      .expect(200)
+      .end(function (err, res) {
+        expect(err).to.equal(null);
+        done();
+      });
+  });
+
+  it('should add post to database', function(done) {
+    request(app).post('/users')
+      .send({username: 'Taylor'})
+      .end(function (err, res) {
+        knex('users').first('id').then(function (id) {
+          request(app).post('/posts')
+          .send({post_content: 'this is a testing post', user_id: id.id, post_link: 'www.google.com'})
+          .end(function (err, res) {
+            knex.select().table('posts').then(function (data) {
+              expect(data[0].post_content).to.include('this is a testing post');
+              done();
+            })
+          });
+        })
+      })
+  });
+
+  it('should find this specific post in the database', function(done) {
+    var postID;
+    var postContent;
+    knex('posts').first('id').then(function (data) {
+      postID = data['id'];
+      postContent = data['post_content'];
+      var param = '/posts/' + postID;
+      request(app).get(param)
+        .end(function (err, res) {
+          expect(res.text).to.include('this is a testing post');
+          done();
+        });
+    })
+  });
+});
+
+describe('comment route', function() {
+  it('should post a comment to specific post page', function(done) {
+    request(app).post('/posts/:postID')
+      .send({comment_content: 'this is a test comment'})
+      .end(function (err, res) {
+        knex('comments').first().then (function (data) {
+          console.log("data:" + data);
+          // expect(res.text).to.include('this is a test comment');
+          done();
+        })
+      });
+  });
+});
+
 describe('/users', function() {
 
   after(function(done) {
-    knex('users').truncate().then(function () {
+    knex.raw('TRUNCATE TABLE users CASCADE').then(function () {
       done();
     });
   });
